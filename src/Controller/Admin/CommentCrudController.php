f02d0197539cc1024e464b4d875f060892c793b8
Herival <herimalala.val@gmail.com>
2023-03-01 16:20:35 +0100

    easyadmin

---
+++ src/Controller/Admin/CommentCrudController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Comment;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class CommentCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Comment::class;
+    }
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            AssociationField::new('conference'),
+            TextField::new('author'),
+            EmailField::new('email'),
+            TextareaField::new('text')->hideOnIndex(),
+            TextField::new('photoFilename'),
+        ];
+    }
+    
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
