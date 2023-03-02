2ba7b8c4cf7a4efa5f4f1cce2d6eca0944ac66ae
Herival <herimalala.val@gmail.com>
2023-03-02 09:51:21 +0100

    configure database sessions

--- migrations/Version20230301125049.php
+++ migrations/Version20230301125049.php
@@ -24,6 +24,9 @@ final class Version20230301125049 extends AbstractMigration
         $this->addSql('CREATE TABLE conference (id INT AUTO_INCREMENT NOT NULL, city VARCHAR(255) NOT NULL, year VARCHAR(4) NOT NULL, is_international TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C604B8382 FOREIGN KEY (conference_id) REFERENCES conference (id)');
+        // Ny BYTEA amin'ny postgre dia BLOB amin'ny My SQL, mba tadidio zay azafady!!!!!
+        $this->addSql('CREATE TABLE sessions (sess_id VARCHAR(128) NOT NULL PRIMARY KEY,sess_data BLOB NOT NULL,sess_lifetime INTEGER NOT NULL,sess_time INTEGER NOT NULL)');
+        $this->addSql('CREATE INDEX expiry ON sessions(sess_lifetime)');
     }
 
     public function down(Schema $schema): void
